627a9bcb6ebb3fd375a48a6cc9a6bf8d4b3af786
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-06-03 11:13:40 +0200

    Generate classes based on DB tables

---
+++ ShopApp/DB/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopApp.DB
+{
+    public partial class Position
+    {
+        public Position()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int Id { get; set; }
+        public string PositionName { get; set; } = null!;
+        public int ShopId { get; set; }
+
+        public virtual Shop Shop { get; set; } = null!;
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ ShopApp/DB/Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopApp.DB
+{
+    public partial class Shop
+    {
+        public Shop()
+        {
+            Employees = new HashSet<Employee>();
+            Positions = new HashSet<Position>();
+        }
+
+        public int Id { get; set; }
+        public string? ShopName { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<Position> Positions { get; set; }
+    }
+}
