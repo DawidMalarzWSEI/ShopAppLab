f136ccb28bb56acb3a499a97c6402cb0db558e83
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-06-03 17:23:53 +0200

    Create views responsible for employees

---
+++ ShopApp/ViewModels/EmployeeDetailModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.ViewModels
+{
+    public class EmployeeDetailModel
+    {
+        public int Id { get; set; }
+        public int UserNo { get; set;  }
+        public string Name { get; set; }
+
+        public string Surename { get; set; }
+
+        public int PositionId { get; set; }
+        public int ShopId { get; set; }
+
+        public int Salary { get; set; }
+        public bool isAdmin { get; set; }
+
+        public DateTime BirthDay { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+    }
+}
